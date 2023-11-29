f209d93b6c21ff9bd0f1eb90c569e3c9a9a112ec
nayeongdev <nayeongkim.dev@gmail.com>
2023-11-29 10:59:56 +0900

    :sparkles: Feat: 회원가입 기능 추가
    회원가입 API 연결
    - email, username, password, password2 필수 항목
    - 그 외는 값을 안받으면 null 처리
    - email이나 username이 중복되면 회원가입 실패

--- js/account/signup.js
+++ js/account/signup.js
@@ -1,43 +1,58 @@
-const $ = (selector) => document.querySelector(selector);
+BASE_URL = "http://127.0.0.1:8000/accounts"
 
-// 메뉴 버튼
-//열기
-$(".menu-open-btn").addEventListener('click', () => {
-  document.body.classList.add("non-scroll");
-  $(".gnb").classList.add("opened");
-  $(".menu-close-btn").classList.add("opened");
-  $("#backdrop").style.display = 'block';
-  document.body.classList.add("non-scroll");
-});
-//닫기
-$('.menu-close-btn').addEventListener('click', () => {
-  $("#backdrop").style.display = 'none';
-  $(".gnb").classList.remove("opened");
-  $(".menu-close-btn").classList.remove("opened");
-  document.body.classList.remove("non-scroll");
-});
+//확인 알럿
+function okAlert(msg) {
+  Swal.fire(msg)
+}
 
-function logoutUser() {
+// 로그인 정보를 받아 서버로 전송하여 JWT를 받아오는 함수
+async function signupUser(signupData) {
+  console.log(JSON.stringify(signupData));
   try {
-    localStorage.removeItem('tokens');
-    location.reload()
+    const response = await fetch(`${BASE_URL}/join/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(signupData)
+    });
+
+    if (!response.ok) {
+      throw new Error('Signup failed');
+    }
+
+    const data = await response.json();
+
+    return data;
   } catch (error) {
-    console.error('Logout error:', error.message);
+    console.error('Signup error:', error.message);
+    return null;
   }
 }
 
-function updateUI() {
-  const hasTokens = localStorage.getItem('tokens');
-  if (hasTokens) {
-    document.getElementById('logged-in').style.display = 'block';
-    document.getElementById('logged-out').style.display = 'none';
+// 회원가입 버튼 클릭 이벤트 핸들러
+document.getElementById('signupBtn').addEventListener('click', async (e) => {
+  e.preventDefault();
+
+  const signupData = {
+    email: $("#userEmail").value,
+    username: $("#username").value,
+    password: $("#userPwd").value,
+    password2: $("#userPwd2").value,
+    date_of_birth: $("#userBirthday").value === '' ? null : $("#userBirthday").value,
+    height: $("#userHeight").value === '' ? null : $("#userHeight").value,
+    weight: $("#userWeight").value === '' ? null : $("#userWeight").value,
+    gender: $('input[name ="user-preferred-activity"]:checked')
+  }
+
+  const data = await signupUser(signupData);
+
+  if (data) {
+    console.log('회원가입 성공');
+    window.location.replace(`http://127.0.0.1:5500/templates/account/login.html`)
   } else {
-    document.getElementById('logged-in').style.display = 'none';
-    document.getElementById('logged-out').style.display = 'block';
+    console.log('회원가입 실패');
+    alertMsg = '회원가입 실패. 다시 확인해주세요.';
+    okAlert(alertMsg);
   }
-}
-
-// 페이지 로드 시 UI 업데이트
-window.addEventListener('DOMContentLoaded', function () {
-  updateUI();
 });
